dfbbfecc83d50737329d3a501429a32b6ddd8b3f
freewind <nowindlee@gmail.com>
2019-04-06 16:49:16 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-Kotlin Hello World Demo
-=======================
+Kotlin Data Structure Binary Tree Demo
+======================================
+
+二叉树的特点：
+
+1. 一个节点的子节点的数量不能超过两个，只能为：0, 1, 2
+2. 子节点有顺序：left, right
 
 Run `Hello.kt` in your IDE.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,19 @@
 package example
 
-fun main(args: Array<String>) {
-    println(hello("Kotlin"))
-}
+data class TreeNode(val data: String, val left: TreeNode? = null, val right: TreeNode? = null)
 
-fun hello(name: String): String = "Hello, $name!"
+fun main(args: Array<String>) {
+    val tree = TreeNode("root",
+            left = TreeNode("aaa",
+                    left = TreeNode("ccc",
+                            left = TreeNode("ggg")
+                    ),
+                    right = TreeNode("ddd", right = TreeNode("hhh"))
+            ),
+            right = TreeNode("bbb",
+                    left = TreeNode("eee"),
+                    right = TreeNode("fff", left = TreeNode("iii"))
+            )
+    )
+    println(tree)
+}
